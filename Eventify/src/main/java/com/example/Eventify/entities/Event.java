65a253c5993555680833b175be63c2b161eaf8a2
alejandro <droguelios@gmail.com>
2026-05-11 08:15:23 -0500

    Feature: I added Entity and table to the topic abot sql and jpa

--- Eventify/src/main/java/com/example/Eventify/entities/Event.java
+++ Eventify/src/main/java/com/example/Eventify/entities/Event.java
@@ -1,16 +1,29 @@
 package com.example.Eventify.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "events")
 public class Event {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
+    @Column(name = "date", nullable = false, length = 50)
     private String date;
+    @Column(name = "description", nullable = false, length = 50)
     private String description;
 }
