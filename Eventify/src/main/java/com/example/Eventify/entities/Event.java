0ff9a441cd93af3d131242b294b58cb18261b119
alejandro <droguelios@gmail.com>
2026-05-29 08:28:06 -0500

    feat: integrate Bootstrap 5.3.3 and clean IDE files

--- Eventify/src/main/java/com/example/Eventify/entities/Event.java
+++ Eventify/src/main/java/com/example/Eventify/entities/Event.java
@@ -1,18 +1,22 @@
 package com.example.Eventify.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+import java.util.HashSet;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.Set;
+
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SQLRestriction("active = true")
+@SQLDelete(sql = "UPDATE events SET active = false WHERE id = ?")
 @Entity
 @Table(name = "events")
 public class Event {
@@ -23,7 +27,23 @@ public class Event {
     @Column(nullable = false, length = 100)
     private String name;
     @Column(nullable = false)
+    private boolean estado = true;
+    @Column(nullable = false)
     private String date;
 
+
     private String description;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "event,categories" ,
+    joinColumns = @JoinColumn(name = "event_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+
+
+
 }
